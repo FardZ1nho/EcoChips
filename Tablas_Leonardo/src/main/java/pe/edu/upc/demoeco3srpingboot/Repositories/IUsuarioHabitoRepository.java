047602d0b4510b4069c4927bb8cd9e8297d9f1ba
FardZ1nho <147080871+FardZ1nho@users.noreply.github.com>
2025-09-17 00:03:25 -0500

    Delete Tablas_Leonardo directory

--- Tablas_Leonardo/src/main/java/pe/edu/upc/demoeco3srpingboot/Repositories/IUsuarioHabitoRepository.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upc.demoeco3srpingboot.Repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pe.edu.upc.demoeco3srpingboot.Entities.UsuarioHabito;
-
-@Repository
-public interface IUsuarioHabitoRepository extends JpaRepository<UsuarioHabito,Integer> {
-}
--- Tablas_Leonardo/src/main/java/pe/edu/upc/demoeco3srpingboot/Repositories/IUsuarioRecompensaRepository.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upc.demoeco3srpingboot.Repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pe.edu.upc.demoeco3srpingboot.Entities.UsuarioRecompensa;
-
-@Repository
-public interface IUsuarioRecompensaRepository extends JpaRepository<UsuarioRecompensa, Integer> {
-}
