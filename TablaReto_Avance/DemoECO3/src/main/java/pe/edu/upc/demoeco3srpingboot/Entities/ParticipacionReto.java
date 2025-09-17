16108ee6fc89984d42ae0fe93754810d07fb89e5
Vianka Aracelli Graciel Diaz Dueñas <u20221g118@upc.edu.pe>
2025-09-16 23:08:16 -0500

    Create ParticipacionReto.java

---
+++ TablaReto_Avance/DemoECO3/src/main/java/pe/edu/upc/demoeco3srpingboot/Entities/ParticipacionReto.java
@@ -0,0 +1,74 @@
+package pe.edu.upc.demoeco3springboot.Entities;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "ParticipacionReto")
+public class ParticipacionReto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idParticipacion;
+
+    @ManyToOne
+    @JoinColumn(name = "idUsuario", nullable = false)
+    private Usuario usuario;
+
+    @ManyToOne
+    @JoinColumn(name = "idReto", nullable = false)
+    private Reto reto;
+
+    private LocalDate fechaRegistro;
+
+    private String estado;
+
+    private int puntosObtenidos;
+
+    public int getIdParticipacion() {
+        return idParticipacion;
+    }
+
+    public void setIdParticipacion(int idParticipacion) {
+        this.idParticipacion = idParticipacion;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public Reto getReto() {
+        return reto;
+    }
+
+    public void setReto(Reto reto) {
+        this.reto = reto;
+    }
+
+    public LocalDate getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(LocalDate fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public int getPuntosObtenidos() {
+        return puntosObtenidos;
+    }
+
+    public void setPuntosObtenidos(int puntosObtenidos) {
+        this.puntosObtenidos = puntosObtenidos;
+    }
+}
