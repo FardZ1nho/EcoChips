6ae5ab3bac7666769a47970f9bd4c9711caa69a7
Vianka Aracelli Graciel Diaz Dueñas <u20221g118@upc.edu.pe>
2025-10-03 01:35:01 -0500

    Update IParticipacionRetoRepository.java

--- TablaReto_Avance/DemoECO3/src/main/java/pe/edu/upc/demoeco3srpingboot/Repositories/IParticipacionRetoRepository.java
+++ TablaReto_Avance/DemoECO3/src/main/java/pe/edu/upc/demoeco3srpingboot/Repositories/IParticipacionRetoRepository.java
@@ -1,10 +1,9 @@
-package pe.edu.upc.demoeco3springboot.Repositories;
+package com.github.fardz1nho.ecochips.repositories;
 
+import com.github.fardz1nho.ecochips.entities.ParticipacionReto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import pe.edu.upc.demoeco3springboot.Entities.ParticipacionReto;
 
 @Repository
 public interface IParticipacionRetoRepository extends JpaRepository<ParticipacionReto, Integer> {
 }
-
