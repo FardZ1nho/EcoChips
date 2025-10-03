228bf79d73552b7ac901970c740c9e2478bab879
Vianka Aracelli Graciel Diaz Dueñas <u20221g118@upc.edu.pe>
2025-10-03 01:50:43 -0500

    Update LogroController.java

--- TablaReto_Avance/DemoECO3/src/main/java/pe/edu/upc/demoeco3srpingboot/Controllers/LogroController.java
+++ TablaReto_Avance/DemoECO3/src/main/java/pe/edu/upc/demoeco3srpingboot/Controllers/LogroController.java
@@ -1,12 +1,18 @@
-package pe.edu.upc.demoeco3springboot.Controllers;
+package com.github.fardz1nho.ecochips.controllers;
 
+import com.github.fardz1nho.ecochips.dtos.LogroDTO;
+import com.github.fardz1nho.ecochips.dtos.ParticipacionRetoDTO;
+import com.github.fardz1nho.ecochips.entities.Logro;
+import com.github.fardz1nho.ecochips.entities.ParticipacionReto;
+import com.github.fardz1nho.ecochips.servicesinterfaces.ILogroService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import pe.edu.upc.demoeco3springboot.Entities.Logro;
-import pe.edu.upc.demoeco3springboot.ServiceInterface.ILogroService;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/logros")
@@ -15,22 +21,54 @@ public class LogroController {
     private ILogroService lService;
 
     @PostMapping
-    public void insertar(@RequestBody Logro logro) {
-        lService.insert(logro);
+    public void insertar(@RequestBody LogroDTO dto) {
+        ModelMapper m = new ModelMapper();
+        Logro l= m.map(dto, Logro.class);
+        lService.insert(l);
     }
 
     @GetMapping
-    public List<Logro> listar() {
-        return lService.list();
+    public List<LogroDTO> listar(){
+        return lService.list().stream().map(y->{
+            ModelMapper m = new ModelMapper();
+            return m.map(y,LogroDTO.class);
+        }).collect(Collectors.toList());
     }
 
     @DeleteMapping("/{id}")
-    public void eliminar(@PathVariable("id") Integer id) {
+    public ResponseEntity<String> Delete(@PathVariable("id") Integer id) {
+        Logro lo = lService.listId(id);
+        if (lo == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("No existe un Logro con el ID: " + id);
+        }
         lService.delete(id);
+        return ResponseEntity.ok("Logro con ID " + id + " eliminado correctamente.");
     }
 
     @GetMapping("/{id}")
-    public Logro listarId(@PathVariable("id") Integer id) {
-        return lService.listId(id);
+    public ResponseEntity<?> listaId(@PathVariable("id") Integer id) {
+        Logro lo = lService.listId(id);
+        if (lo == null) {
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .body("No existe una recompensa con el ID: " + id);
+        }
+        ModelMapper m = new ModelMapper();
+        LogroDTO dto = m.map(lo, LogroDTO.class);
+        return ResponseEntity.ok(dto);
+    }
+
+    @PutMapping
+    public ResponseEntity<String> update(@RequestBody LogroDTO dto) {
+        ModelMapper m = new ModelMapper();
+        Logro lo = m.map(dto,Logro.class);
+        Logro existe = lService.listId(lo.getIdLogro());
+        if(existe==null){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("No se puede modificar. No existe un logro con el ID: " + lo.getIdLogro());
+        }
+        lService.update(lo);
+        return ResponseEntity.ok("Logro con ID " + lo.getIdLogro() + " modificado correctamente.");
     }
 }
