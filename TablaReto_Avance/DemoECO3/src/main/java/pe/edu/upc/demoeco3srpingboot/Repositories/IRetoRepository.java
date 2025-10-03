da91ecbd4e20ddd9828268628aefbad07da00e49
Vianka Aracelli Graciel Diaz Dueñas <u20221g118@upc.edu.pe>
2025-10-03 01:35:25 -0500

    Update IRetoRepository.java

--- TablaReto_Avance/DemoECO3/src/main/java/pe/edu/upc/demoeco3srpingboot/Repositories/IRetoRepository.java
+++ TablaReto_Avance/DemoECO3/src/main/java/pe/edu/upc/demoeco3srpingboot/Repositories/IRetoRepository.java
@@ -1,11 +1,21 @@
-package pe.edu.upc.demoeco3springboot.Repositories;
+package com.github.fardz1nho.ecochips.repositories;
 
+import com.github.fardz1nho.ecochips.entities.Reto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import pe.edu.upc.demoeco3springboot.Entities.Reto;
 
+import java.time.LocalDate;
 import java.util.List;
 
+
 @Repository
-public interface IRetoRepository extends JpaRepository<Reto, Long> {
+public interface IRetoRepository extends JpaRepository<Reto, Integer> {
+    @Query("SELECT COUNT(r) " +
+            "FROM Reto r " +
+            "WHERE r.fechaInicio >= :fechaInicio " +
+            "AND r.fechaFin <= :fechaFin")
+    int countRetosByFechas(@Param("fechaInicio") LocalDate fechaInicio,
+                           @Param("fechaFin") LocalDate fechaFin);
 }
