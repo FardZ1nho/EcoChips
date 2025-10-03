2d5d7066b876acdb2f584afb78b0e900f5f3f407
Vianka Aracelli Graciel Diaz Dueñas <u20221g118@upc.edu.pe>
2025-10-03 01:46:11 -0500

    Update ILogroService.java

--- TablaReto_Avance/DemoECO3/src/main/java/pe/edu/upc/demoeco3srpingboot/ServiceInterface/ILogroService.java
+++ TablaReto_Avance/DemoECO3/src/main/java/pe/edu/upc/demoeco3srpingboot/ServiceInterface/ILogroService.java
@@ -1,12 +1,14 @@
-package pe.edu.upc.demoeco3springboot.ServiceInterface;
+package com.github.fardz1nho.ecochips.servicesinterfaces;
 
-import pe.edu.upc.demoeco3springboot.Entities.Logro;
+
+import com.github.fardz1nho.ecochips.entities.Logro;
 
 import java.util.List;
 
 public interface ILogroService {
     public void insert(Logro logro);
     public List<Logro> list();
-    public void delete(int idLogro);
-    public Logro listId(int idLogro);
+    public void delete(int id);
+    public Logro listId(int id);
+    void update(Logro logro);
 }
