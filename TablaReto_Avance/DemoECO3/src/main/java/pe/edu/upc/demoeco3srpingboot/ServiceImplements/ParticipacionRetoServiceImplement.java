55d44d3f23df185b12c04fcc57cc2bc40cdbd6f1
Vianka Aracelli Graciel Diaz Dueñas <u20221g118@upc.edu.pe>
2025-10-03 01:41:52 -0500

    Update ParticipacionRetoServiceImplement.java

--- TablaReto_Avance/DemoECO3/src/main/java/pe/edu/upc/demoeco3srpingboot/ServiceImplements/ParticipacionRetoServiceImplement.java
+++ TablaReto_Avance/DemoECO3/src/main/java/pe/edu/upc/demoeco3srpingboot/ServiceImplements/ParticipacionRetoServiceImplement.java
@@ -1,15 +1,16 @@
-package pe.edu.upc.demoeco3springboot.ServiceImplements;
+package com.github.fardz1nho.ecochips.serviceimplements;
 
+import com.github.fardz1nho.ecochips.entities.ParticipacionReto;
+import com.github.fardz1nho.ecochips.repositories.IParticipacionRetoRepository;
+import com.github.fardz1nho.ecochips.servicesinterfaces.IParticipacionRetoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import pe.edu.upc.demoeco3springboot.Entities.ParticipacionReto;
-import pe.edu.upc.demoeco3springboot.Repositories.IParticipacionRetoRepository;
-import pe.edu.upc.demoeco3springboot.ServiceInterface.IParticipacionRetoService;
+
 
 import java.util.List;
 
 @Service
-public class ParticipacionRetoServiceImplement implements IParticipacionRetoService{
+public class ParticipacionRetoServiceImplement implements IParticipacionRetoService {
     @Autowired
     private IParticipacionRetoRepository prRepo;
 
@@ -19,7 +20,22 @@ public class ParticipacionRetoServiceImplement implements IParticipacionRetoServ
     }
 
     @Override
+    public ParticipacionReto listId(int id){
+        return prRepo.findById(id).orElse(new ParticipacionReto());
+    }
+
+    @Override
     public void insert(ParticipacionReto participacionReto){
         prRepo.save(participacionReto);
+    }
+
+    @Override
+    public void update(ParticipacionReto participacionReto) {
+        prRepo.save(participacionReto);
+    }
+
+    @Override
+    public void delete(int id) {
+        prRepo.deleteById(id);
     }
 }
