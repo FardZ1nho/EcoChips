b62f7ec9c75e65a3f696f7d337d02fb923b2177d
Vianka Aracelli Graciel Diaz Dueñas <u20221g118@upc.edu.pe>
2025-10-03 01:35:43 -0500

    Update ITipoRetoRepository.java

--- TablaReto_Avance/DemoECO3/src/main/java/pe/edu/upc/demoeco3srpingboot/Repositories/ITipoRetoRepository.java
+++ TablaReto_Avance/DemoECO3/src/main/java/pe/edu/upc/demoeco3srpingboot/Repositories/ITipoRetoRepository.java
@@ -1,11 +1,19 @@
-package pe.edu.upc.demoeco3springboot.Repositories;
+package com.github.fardz1nho.ecochips.repositories;
 
+import com.github.fardz1nho.ecochips.entities.TipoReto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import pe.edu.upc.demoeco3springboot.Entities.TipoReto;
 
 import java.util.List;
 
 @Repository
-public interface ITipoRetoRepository extends JpaRepository<TipoReto, Long> {
+public interface ITipoRetoRepository extends JpaRepository<TipoReto, Integer> {
+    @Query(value = "SELECT tr.nombre_tipo_reto, \n" +
+            "       COUNT(r.id_reto) AS TotalRetos\n" +
+            "FROM tipo_reto tr\n" +
+            "LEFT JOIN reto r ON tr.id_tipo_reto = r.id_tipo_reto\n" +
+            "GROUP BY tr.nombre_tipo_reto\n" +
+            "ORDER BY TotalRetos DESC", nativeQuery = true)
+    List<String[]>TiposXretos();
 }
