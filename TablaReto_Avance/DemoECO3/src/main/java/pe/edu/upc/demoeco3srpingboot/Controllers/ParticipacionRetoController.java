9bb3aef2c701a310c583b63f281d4f2e20f2eb40
Vianka Aracelli Graciel Diaz Dueñas <u20221g118@upc.edu.pe>
2025-10-03 01:51:28 -0500

    Update ParticipacionRetoController.java

--- TablaReto_Avance/DemoECO3/src/main/java/pe/edu/upc/demoeco3srpingboot/Controllers/ParticipacionRetoController.java
+++ TablaReto_Avance/DemoECO3/src/main/java/pe/edu/upc/demoeco3srpingboot/Controllers/ParticipacionRetoController.java
@@ -1,11 +1,14 @@
-package pe.edu.upc.demoeco3springboot.Controllers;
+package com.github.fardz1nho.ecochips.controllers;
 
+import com.github.fardz1nho.ecochips.dtos.ParticipacionRetoDTO;
+import com.github.fardz1nho.ecochips.entities.ParticipacionReto;
+import com.github.fardz1nho.ecochips.servicesinterfaces.IParticipacionRetoService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import pe.edu.upc.demoeco3springboot.DTOs.ParticipacionRetoDTO;
-import pe.edu.upc.demoeco3springboot.Entities.ParticipacionReto;
-import pe.edu.upc.demoeco3springboot.ServiceInterface.IParticipacionRetoService;
+
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -18,10 +21,10 @@ public class ParticipacionRetoController {
 
     @GetMapping
     public List<ParticipacionRetoDTO> listar() {
-        ModelMapper m = new ModelMapper();
-        return prService.list().stream().map(x -> m.map(x,
-                        ParticipacionRetoDTO.class))
-                .collect(Collectors.toList());
+        return prService.list().stream().map(x->{
+            ModelMapper m = new ModelMapper();
+            return m.map(x, ParticipacionRetoDTO.class);
+        }).collect(Collectors.toList());
     }
 
     @PostMapping
@@ -29,5 +32,29 @@ public class ParticipacionRetoController {
         ModelMapper m = new ModelMapper();
         ParticipacionReto p = m.map(dto, ParticipacionReto.class);
         prService.insert(p);
+    }
+
+    @PutMapping
+    public ResponseEntity<String> update(@RequestBody ParticipacionRetoDTO dto) {
+        ModelMapper m = new ModelMapper();
+        ParticipacionReto p = m.map(dto,ParticipacionReto.class);
+        ParticipacionReto existe = prService.listId(p.getIdParticipacion());
+        if(existe==null){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("No se puede modificar. No existe un registro con el ID: " + p.getIdParticipacion());
+        }
+        prService.update(p);
+        return ResponseEntity.ok("Participacion con ID " + p.getIdParticipacion() + " modificado correctamente.");
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") Integer id) {
+        ParticipacionReto dev = prService.listId(id);
+        if(dev==null){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("No existe un participación con el ID: " + id);
+        }
+        prService.delete(id);
+        return ResponseEntity.ok("Participación con el ID: " + id + " eliminado correctamente.");
     }
 }
