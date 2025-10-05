f18b1f82bd33ed49e4952bca527fd9bd216546d3
FardZ1nho <147080871+FardZ1nho@users.noreply.github.com>
2025-10-05 14:15:04 -0500

    Agrega configuración de OpenAPI con seguridad JWT

---
+++ src/main/java/pe/edu/upc/demoeco3srpingboot/securities/OpenApiConfig.java
@@ -0,0 +1,27 @@
+package pe.edu.upc.demoeco3srpingboot.securities;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "DemoECO3",
+                version = "1.0",
+                description = "Documentación de tu API protegida con JWT"
+        ),
+        security = {
+                @SecurityRequirement(name = "bearerAuth")
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+public class OpenApiConfig {
+}
