4c590b1ffc02b0bf6c5705a644172ed4980ec349
Ferny <fernando.dotlj@gmail.com>
2025-10-10 09:35:42 -0500

    Update UsuarioRecompensaDTO.java

--- src/main/java/pe/edu/upc/demoeco3srpingboot/DTOs/UsuarioRecompensaDTO.java
+++ src/main/java/pe/edu/upc/demoeco3srpingboot/DTOs/UsuarioRecompensaDTO.java
@@ -6,14 +6,9 @@ import pe.edu.upc.demoeco3srpingboot.Entities.Usuario;
 import java.time.LocalDate;
 
 public class UsuarioRecompensaDTO {
-    private int idUsuarioRecompensa;
     private LocalDate fechaCanje;
     private int idUsuario;
-    private int idRecompensa;
 
-    public int getIdUsuarioRecompensa() {return idUsuarioRecompensa;}
-
-    public void setIdUsuarioRecompensa(int idUsuarioRecompensa) {this.idUsuarioRecompensa = idUsuarioRecompensa;}
 
     public LocalDate getFechaCanje() {return fechaCanje;}
 
@@ -22,8 +17,4 @@ public class UsuarioRecompensaDTO {
     public int getIdUsuario() {return idUsuario;}
 
     public void setIdUsuario(int idUsuario) {this.idUsuario = idUsuario;}
-
-    public int getIdRecompensa() {return idRecompensa;}
-
-    public void setIdRecompensa(int idRecompensa) {this.idRecompensa = idRecompensa;}
 }
